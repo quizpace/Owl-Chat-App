5db22c947ca250238014987e85a83d0b0b0260ac
Avi <drazninavi@gmail.com>
2023-11-26 18:25:50 +0200

    a

---
+++ upload-pic.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const uploadImage = document.getElementById("pic-upload");
+  const fileInput = document.getElementById("fileInput");
+
+  uploadImage.addEventListener("click", function () {
+    fileInput.click(); // Trigger click on file input when image is clicked
+  });
+
+  fileInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("photo", file);
+
+    reqwest({
+      url: "https://photos-api-mzpl.onrender.com/upload",
+      method: "POST",
+      processData: false,
+      data: formData,
+      success: function (response) {
+        console.log("Upload successful:", response);
+        // Add any success handling code here
+      },
+      error: function (err) {
+        console.error("Error uploading:", err);
+        // Add any error handling code here
+      },
+    });
+  });
+});
