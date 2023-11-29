1c0750c6c7af6a510d2f272f70e82b6880e4f246
Avi <drazninavi@gmail.com>
2023-11-29 10:40:13 +0200

    ש

--- onload.js
+++ onload.js
@@ -28,5 +28,5 @@ window.onload = async () => {
     if (!chatBox.classList.contains("expanded")) {
       scrollChatBoxDown();
     }
-  }, 10); // Set the interval to 1000 milliseconds (1 second)
+  }, 1000); // Set the interval to 1000 milliseconds (1 second)
 };
